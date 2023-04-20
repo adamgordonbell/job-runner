388826ef7932586a6afb58a2f5f6a01f3faf950b
Adam Gordon Bell <adam@corecursive.com>
2023-04-20 09:28:17 -0400

    WIP

--- job_parser.py
+++ job_parser.py
@@ -1,7 +1,7 @@
 from parsimonious import Grammar, NodeVisitor
 import argparse
 from pprint import pprint
-
+import subprocess
 
 # Define the grammar for the custom job script
 grammar = Grammar(
@@ -20,6 +20,8 @@ grammar = Grammar(
 )
 
 # Define a visitor to process the parsed nodes
+
+
 class JobVisitor(NodeVisitor):
     def visit_job(self, node, visited_children):
         _, _, name, _, parallel, _, _, command, _, _, *rest = visited_children
@@ -28,12 +30,17 @@ class JobVisitor(NodeVisitor):
             "name": name,
             "parallel": parallel == [True],
             "command": command,
-            "condition": rest[0][0] if isinstance(rest[0] , list) else None
+            "condition": rest[0][0] if isinstance(rest[0], list) else None
         }
 
     def visit_wait(self, node, visited_children):
         return {
             "type": "wait",
+        }
+
+    def visit_ws(self, node, visited_children):
+        return {
+            "type": "noop",
         }
 
     def visit_parallel(self, node, visited_children):
@@ -46,10 +53,15 @@ class JobVisitor(NodeVisitor):
         return node.text.strip()
 
     def visit_when(self, node, visited_children):
-         return visited_children[4].text.strip()
+        return visited_children[4].text.strip()
 
     def generic_visit(self, node, visited_children):
         return visited_children or node
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
 
 def main(job_file_path):
     with open(job_file_path, "r") as job_file:
@@ -57,15 +69,49 @@ def main(job_file_path):
 
     parsed_nodes = grammar.parse(job_script)
     visitor = JobVisitor()
-    jobs = visitor.visit(parsed_nodes)
+    jobs = flatten(visitor.visit(parsed_nodes))
 
-    pprint(jobs)
+    # pprint(jobs)
+    runjobs(jobs)
+
+
+def runjobs(jobs):
+    parallel_jobs = []
+    for job in jobs:
+        if job["type"] == "job":
+            execute_job = True
+            if job["condition"]:
+                execute_job = should_execute(job["condition"])
+            if execute_job:
+                if job["parallel"]:
+                    process = subprocess.Popen(job["command"], shell=True)
+                    parallel_jobs.append(process)
+                else:
+                    if parallel_jobs:
+                        for process in parallel_jobs:
+                            process.wait()
+                        parallel_jobs = []
+
+                    run_command(job["command"])
+        elif job["type"] == "wait":
+            if parallel_jobs:
+                for process in parallel_jobs:
+                    process.wait()
+                parallel_jobs = []
+
+def should_execute(command):
+    return run_command(command)
+
+def run_command(command):
+    print(f"Running command: {command}")
+    result = subprocess.run(command, stdout=None, stderr=None, text=True, shell=True)
+    print(f"Result: {result.returncode}")
+    return result.returncode == 0
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run jobs from a custom job script")
+    parser = argparse.ArgumentParser(
+        description="Run jobs from a custom job script")
     parser.add_argument("job_file", help="Path to the job script file")
     args = parser.parse_args()
 
     main(args.job_file)
-
-
