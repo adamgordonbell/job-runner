387605cd2f9a92b931c507aa4d8c4046bc229af8
Adam Gordon Bell <adam@corecursive.com>
2023-04-20 16:09:31 -0400

    fixing job runner

--- job_parser.py
+++ job_parser.py
@@ -2,8 +2,8 @@ from parsimonious import Grammar, NodeVisitor
 import argparse
 from pprint import pprint
 import subprocess
+import concurrent.futures
 
-# Define the grammar for the custom job script
 grammar = Grammar(
     r"""
     job_script = (job / wait / ws)*
@@ -18,9 +18,6 @@ grammar = Grammar(
     ws = ~r"\s*"
     """
 )
-
-# Define a visitor to process the parsed nodes
-
 
 class JobVisitor(NodeVisitor):
     def visit_job(self, node, visited_children):
@@ -58,10 +55,45 @@ class JobVisitor(NodeVisitor):
     def generic_visit(self, node, visited_children):
         return visited_children or node
 
-
 def flatten(l):
     return [item for sublist in l for item in sublist]
 
+def runjobs(jobs):
+    parallel_jobs = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for job in jobs:
+            if job["type"] == "job":
+                execute_job = True
+                if job["condition"]:
+                    execute_job = should_execute(job["condition"])
+                if execute_job:
+                    if job["parallel"]:
+                        future = executor.submit(run_command, job["command"])
+                        parallel_jobs.append(future)
+                    else:
+                        command_success = run_command(job["command"])
+
+                        if not command_success:
+                            print(f'Failure running job: {job["name"]}')
+                            break
+            elif job["type"] == "wait":
+                if parallel_jobs:
+                    bail = False
+                    for f in parallel_jobs:
+                        command_success = future.result()
+                        bail = command_success if not command_success else bail
+                      
+                    parallel_jobs = []
+                    if not command_success:
+                        print(f'Failure running parallel job')
+                        break
+
+def should_execute(command):
+    return run_command(command)
+
+def run_command(command):
+    result = subprocess.run(command, stdout=None, stderr=None, text=True, shell=True)
+    return result.returncode == 0
 
 def main(job_file_path):
     with open(job_file_path, "r") as job_file:
@@ -70,43 +102,7 @@ def main(job_file_path):
     parsed_nodes = grammar.parse(job_script)
     visitor = JobVisitor()
     jobs = flatten(visitor.visit(parsed_nodes))
-
-    # pprint(jobs)
     runjobs(jobs)
-
-
-def runjobs(jobs):
-    parallel_jobs = []
-    for job in jobs:
-        if job["type"] == "job":
-            execute_job = True
-            if job["condition"]:
-                execute_job = should_execute(job["condition"])
-            if execute_job:
-                if job["parallel"]:
-                    process = subprocess.Popen(job["command"], shell=True)
-                    parallel_jobs.append(process)
-                else:
-                    if parallel_jobs:
-                        for process in parallel_jobs:
-                            process.wait()
-                        parallel_jobs = []
-
-                    run_command(job["command"])
-        elif job["type"] == "wait":
-            if parallel_jobs:
-                for process in parallel_jobs:
-                    process.wait()
-                parallel_jobs = []
-
-def should_execute(command):
-    return run_command(command)
-
-def run_command(command):
-    print(f"Running command: {command}")
-    result = subprocess.run(command, stdout=None, stderr=None, text=True, shell=True)
-    print(f"Result: {result.returncode}")
-    return result.returncode == 0
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
